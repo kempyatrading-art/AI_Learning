58b15da58347ddbb7aa51c80d56191afc75df151
RAHUL SANTOSH MUDAKANNAVAR <kempyatrading@gmail.com>
2026-06-06 15:41:51 +0530

    Improved conditions logic

--- day1_python/conditions.py
+++ day1_python/conditions.py
@@ -1,3 +1,4 @@
+"""
 students = {
     "2VX23RA406": {
         "name": "Rahul",
@@ -25,3 +26,30 @@ if usn in students:
     
 else :
     print ("Enterd USN not Found in Database ")
+"""
+
+students ={
+    "2VX23RA406" : {
+        "name" : "Rahul",
+        "branch" : "R.A",
+        "dob" : "24-07-2002",
+        "goal" : "ai eng"
+    },
+    "2VX23RA404" : {
+        "name" : "Raghav",
+        "branch" : "R.A",
+        "dob" : "16-10-2003",
+        "goal" : "ai eng"
+    }
+}
+usn= input("Enter the USN:" )
+if usn in students :
+    student=students [usn]
+    print("\nstudent found")
+    print("Name :", student["name"])
+    print("Branh :", student["branch"])
+    print("DOB :", student["dob"])
+    print("GOAL :", student["goal"])
+else :
+    print("student data not found in database please contect help line number for any quarry")
+    
