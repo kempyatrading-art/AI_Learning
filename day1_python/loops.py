9920c43c56d09f7616eeffc8fe7800008633b74a
RAHUL SANTOSH MUDAKANNAVAR <kempyatrading@gmail.com>
2026-06-06 22:01:14 +0530

    Completed loops practice

---
+++ day1_python/loops.py
@@ -0,0 +1,63 @@
+#1st prog
+"""
+for i in range(5):
+    print(i)
+    """
+
+#2nd prog
+"""
+for i in range (5):
+    print ("Rahul")
+"""
+#3rd prog
+"""
+for i in range (1,6):
+    print(i)
+    """
+# 4 th prog :trading bot style loop
+"""
+for cycle in range (1,6):
+    print ("checking markete cycle:",cycle)
+    """
+#5th program : while loop :-
+"""
+count = 1
+while count <=7:
+    print(count)
+    count+=1
+    """
+
+"""
+count = 1
+while count >=7:
+    print(count)
+    count+=1
+else : 
+    print ("it is greater then 7")
+    
+    """
+
+#Mini Project :
+
+attempts =3
+
+while attempts >0:
+    usn = input ("Enterthe usn :")
+    
+    if usn =="2VX23RA406" :
+        print ("Access Granted")
+        break
+    else :
+        attempts -=1
+        print ("Wrong USN please chuck the usn once and retry")
+        print ("Reamaining Attempts :",attempts)
+        
+        print("End program")
+    
+        
+    
+    
+
+
+    
+
