2a33e6ba3c51d85aa1b7c231fa1b013b3da3b212
RAHUL SANTOSH MUDAKANNAVAR <kempyatrading@gmail.com>
2026-06-18 15:45:46 +0530

    Completed Day 1 mini project

--- day1_python/mini_project.py
+++ day1_python/mini_project.py
@@ -2,13 +2,24 @@ students = {
     "2VX23RA406" :{
         "name" : "Rahul",
         "branch": "Robotics and Automation",
-        "goal" : "AI Engineer"
+        "goal" : "AI Engineer",
+        "cgpa" : "8.18",
+        "skills" : ["Python","AI","Trading","Robotics"]
     },
     
     "2VX23RA404":{
         "name" : "Raghav",
         "branch": "Robotics and Automation",
-        "goal" : "AI Engineer"
+        "goal" : "AI Engineer",
+        "cgpa" : "8.180",
+        "skills" : ["Python","AI","Robotics"]
+    },
+    "2VX23RA405":{
+        "name": "Raghu",
+        "branch": "Robotics and Automation",
+        "goal": "Hardware Engineer",
+        "cgpa": "8.5",
+        "skills":["ROS","C++","Arduino"]
     }
 }
 
@@ -23,6 +34,8 @@ def search_student ():
             print("Name :",student["name"])
             print("Branch :",student["branch"])
             print("Goal :",student["goal"])
+            print("CGPA :",student["cgpa"])
+            print("Skills :", ", ".join(student["skills"]))
             
             return
         else :
@@ -30,5 +43,14 @@ def search_student ():
             print ("usn not found chuck your usn  and try again")
             print("attempts left :",[attempts])
             print("\nacces block")
-search_student()
+            
+while True:
+    search_student()
+    
+    again = input("\nDo you want to search again? (Y/N) : ").upper()
+    if again !="Y":
+        print("Goodbye!")
+        break
+
+
             
