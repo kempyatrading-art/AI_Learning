029e1b39f5a1ac9e9222bd3865a8e41ed0a080a3
RAHUL SANTOSH MUDAKANNAVAR <kempyatrading@gmail.com>
2026-06-27 23:30:06 +0530

    Completed JSON student database search system

--- day2_python/json_basics.py
+++ day2_python/json_basics.py
@@ -29,10 +29,8 @@ print("json file saved successfully")
 """
 
 # Step 4 — Read JSON File
-
-import json
-
 """
+import json
 student = {
     "name" : "Rahul",
     "branch" : "Robotics and Automation",
@@ -66,7 +64,7 @@ print(data["cgpa"])
 print(data["skills"])
 """
 # Mini Project
-
+"""
 import json
 
 student = {
@@ -92,3 +90,102 @@ print ("Name :", data ["name"])
 print ("Branch :", data ["branch"])
 print ("CGPA :", data ["cgpa"])
 print ("Goal :", data ["goal"])
+
+"""
+
+# Mini Project : Next Level: Multiple Students JSON Database
+"""
+import json 
+# 1. Define a database structure with a list of multiple students
+students = {
+    "students": [
+        {
+            "usn": "2VX23RA406",
+            "name": "Rahul",
+            "branch": "Robotics and Automation",
+            "cgpa": 8.18,
+            "goal": "AI Engineer"
+        },
+        {
+            "usn": "2VX23RA404",
+            "name": "Raghav",
+            "branch": "Robotics and Automation",
+            "cgpa": 8.20,
+            "goal": "Robotics Engineer"
+        },
+        {
+            "usn": "2VX23RA405",
+            "name": "Raghu",
+            "branch": "Mechanical Engineering",
+            "cgpa": 8.50,
+            "goal": "Automation Specialist"
+        }
+    ]
+}
+# 2. Write the entire database to a new file 'students.json'
+with open ("students.json","w") as file :
+    json.dump(students,file,indent=4)
+    
+print("Students Database Saved Successfully!\n")
+
+# 2. Write the entire database to a new file 'students.json'
+with open ("students.json","r") as file :
+    data = json.load (file)
+    
+# 4. Loop through the list of students
+print ("----FETCHING STUDENT PROFILES---")
+
+for student in data ["students"] :
+    print ("USN    :",student ["usn"])
+    print ("NAME   :",student ["name"])
+    print ("BRANCH :",student ["branch"])
+    print ("CGPA   :",student ["cgpa"])
+    print ("GOAL   :",student ["goal"])
+    print ("-" *30) #separate line
+"""
+# Mini Project : Instead of printing every student, ask the user for a USN.
+import json
+with open ("students.json","r") as file :
+    data = json.load(file)
+    
+print ("--- STUDENT DATABASE SEARCH SYSTEM ---")
+while True :
+    
+    search_usn = input ("Enter the USN :").upper()
+    found = False 
+
+    for student in data ["students"] :
+        if student["usn"] == search_usn :
+            print ("\n student profile found :")
+            print ("-" * 30)
+            print ("USN    :",student ["usn"])
+            print ("NAME   :",student ["name"])
+            print ("BRANCH :",student ["branch"])
+            print ("CGPA   :",student ["cgpa"])
+            print ("GOAL   :",student ["goal"])
+            print ("-" *30) #separate line
+            
+            found = True
+            
+            break
+    if not found :
+            print (f"\n no student found with this USN : {search_usn}") 
+            
+    while True:
+
+            choice = input("\nDo you want to search again? (Y/N): ").upper()
+
+            if choice == "Y":
+                break          # leave this small loop and search again
+
+            elif choice == "N":
+                print("Thank you for using Student Database.")
+                exit()         # end the program
+
+            else:
+                print("Invalid choice. Please enter only Y or N.")
+            
+            
+            
+            
+            
